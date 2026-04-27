b9c9ed813933e94a6ed96cda5758325d661f192e
Angel <Angel@Angel>
2026-04-27 18:59:59 -0500

    commit 2

---
+++ src/main/java/Paciente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Angel
+ */
+public class Paciente {
+    private String id;
+    private String nombre;
+    private String tipoSangre;
+    private String email;
+    
+    public Paciente(String id, String nombre, String tipoSangre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipoSangre = tipoSangre;
+        this.email = email;
+    }
+    
+}
