5c147a7c2a46e4c0fd725936b8bf2236f014d5be
Angel <Angel@Angel>
2026-04-27 18:44:18 -0500

    commit inicial

---
+++ src/main/java/com/mycompany/t1/tecnicas/de/program/orie/obj/T1TECNICASDEPROGRAMORIEOBJ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.t1.tecnicas.de.program.orie.obj;
+
+/**
+ *
+ * @author Angel
+ */
+public class T1TECNICASDEPROGRAMORIEOBJ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
