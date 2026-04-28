e38ba47b7e29bc8e1a1fe438cd7ebec4b57dbaae
Angel <Angel@Angel>
2026-04-27 19:02:16 -0500

    commit update

--- src/main/java/com/mycompany/t1/tecnicas/de/program/orie/obj/Paciente.java
+++ src/main/java/com/mycompany/t1/tecnicas/de/program/orie/obj/Paciente.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.t1.tecnicas.de.program.orie.obj;
 
 /**
  *
@@ -12,12 +13,14 @@ public class Paciente {
     private String nombre;
     private String tipoSangre;
     private String email;
-    
+
     public Paciente(String id, String nombre, String tipoSangre, String email) {
         this.id = id;
         this.nombre = nombre;
         this.tipoSangre = tipoSangre;
         this.email = email;
     }
+
+    public String getNombre() { return nombre; }
     
 }
